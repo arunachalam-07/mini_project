f9f57fa9db17cc4d1d88795c656c453fb85946f3
arunachalam <sarun1099@gmail.com>
2020-01-27 20:34:41 +0530

    updatiion

--- logout.php
+++ logout.php
@@ -1,6 +1,8 @@
 <?php
 session_start();
+echo $_SESSION['name'];
 echo $_SESSION['pass'];
 session_destroy();
 //session_destroy();
+echo 'you are logged out';
 ?>
--- main.php
+++ main.php
@@ -4,7 +4,7 @@ if(isset($_SESSION['pass'])){
 echo 'you are logged in';
 }else{
     session_destroy();
-    //header('Location:./in.php');
+    header('Location:./in.php');
    //header('Location:/nodes/in.php');
    //echo "<script> location.href='in.php'; </script>";
 }
